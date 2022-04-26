de099554f4b2fbf3196cba3c3563c51b763009e2
Cherie Menchaca <menchaca.cherie@student.greenriver.edu>
2022-04-26 15:59:18 -0400

    modified the following files index and views

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start session
+session_start();
+
 //Require the autoload File
 require_once ('vendor/autoload.php');
 
@@ -20,22 +23,43 @@ $f3->route('GET /', function(){
 //Define a breakfast route
 $f3->route('GET /breakfast', function(){
     //echo "Breakfast page";
-    //echo "Diner Project";
+
     $view = new Template();
     echo $view->render('views/breakfast.html');
 });
 $f3->route('GET /lunch', function(){
-    //echo "Breakfast page";
-    //echo "Diner Project";
+
     $view = new Template();
     echo $view->render('views/lunch.html');
 });
+//Define an order route
 $f3->route('GET /order', function(){
-    //echo "Breakfast page";
-    //echo "Diner Project";
+    //echo "Order Form"
     $view = new Template();
-    echo $view->render('views/order.html');
+    echo $view->render('views/orderForm1.html');
+});
+//Define an order part# 2
+$f3->route('POST /order2', function(){
+    var_dump($_POST);
+    $_SESSION['food'] = $_POST['food'];
+    $_SESSION['meal'] = $_POST['meal'];
+    $view = new Template();
+    echo $view->render('views/orderForm2.html');
 });
 
+//Define a summary route
+$f3->route('POST /summary', function(){
+    var_dump($_POST);
+    if(empty($_POST['conds']))
+    {
+        $conds = "";
+    }
+    else{
+        $conds = implode(", ", $_POST['conds']);
+    }
+    $_SESSION['conds'] = $conds;
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
 //Run fat free
 $f3->run();
